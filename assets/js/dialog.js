0f01eb8ec983cea28fe23be9150e03d45f386033
Ioannis Valasakis <code@wizofe.uk>
2019-06-09 14:36:18 +0100

    Main gui implementation

---
+++ assets/js/dialog.js
@@ -0,0 +1,62 @@
+const { ipcRenderer } = require('electron')
+
+ipcRenderer.on('open-dialog-paths-selected', (event, arg)=> {
+  dialog.handler.outputSelectedPathsFromOpenDialog(arg);
+})
+
+ipcRenderer.on('show-message-box-response', (event, args) => {
+  dialog.handler.outputMessageboxResponse(args);
+})
+
+window.dialog = window.dialog || {},
+function(n) {
+
+    dialog.handler = {
+
+      showOpenDialog: function() {
+        ipcRenderer.send('show-open-dialog');
+      },
+
+      outputSelectedPathsFromOpenDialog: function(paths) {
+        alert('user selected: ' + paths);
+      },
+
+      outputMessageboxResponse: function(args) {
+        alert('user selected button index: ' + args[0] + '. Should remember answer value is: ' + args[1]);
+      },
+
+      showErrorBox: function() {
+        ipcRenderer.send('show-error-box');
+      },
+
+      showMessageBox: function() {
+        ipcRenderer.send('show-message-box');
+      },
+
+      showSaveDialog: function() {
+        ipcRenderer.send('show-save-dialog');
+      },
+
+      init: function() {
+        $('#showOpendialog').click( function () {
+          dialog.handler.showOpenDialog();
+        })
+
+        $('#showErrorBox').click( function () {
+          dialog.handler.showErrorBox();
+        })
+
+        $('#showMessageBox').click( function() {
+          dialog.handler.showMessageBox();
+        })
+
+        $('#showSaveDialog').click( function() {
+          dialog.handler.showSaveDialog();
+        })
+      }
+    };
+
+    n(function() {
+        dialog.handler.init();
+    })
+}(jQuery);
---
+++ run-all.bat
@@ -0,0 +1,3 @@
+@echo off
+start "" "Run-dash.bat"
+start "" "Run-electron.bat"
